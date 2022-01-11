abfebaf3262ff2a2e36f67dfcf3fd8bb5256b4ac
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2022-01-11 20:09:37 -0300

    sanvando conteudo

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -47,7 +47,48 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+
+    selector = Selector(text=html_content)
+    data = {}
+
+    data["url"] = selector.css("head > link[rel=canonical] ::attr(href)").get()
+    data["title"] = selector.css("#js-article-title ::text").get()
+    data["timestamp"] = selector.css("#js-article-date ::attr(datetime)").get()
+    data["writer"] = (
+        selector.css(".tec--author__info__link ::text").get().strip()
+    )
+    write = selector.css(".tec--author__info__link ::text").get().strip()
+    data["shares_count"] = int(
+        selector.css(".tec--toolbar > div:nth-child(1) ::text")
+        .get()
+        .split()[0]
+    )
+    data["comments_count"] = int(selector.css(
+        "#js-comments-btn ::attr(data-count)"
+    ).get())
+    data["summary"] = "".join(
+        selector.css(
+            ".tec--article__body.z--px-16.p402_premium > p:nth-child(1) ::text"
+        ).getall()
+    )
+    select_sources = selector.css(".z--mb-16.z--px-16 > div ::text").getall()
+    remove_spaces_sources = list(
+        map(lambda x: x.strip(), select_sources)
+    )
+    data["sources"] = list(filter(lambda x: x != "", remove_spaces_sources))
+    select_categories = selector.css("#js-categories ::text").getall()
+    remove_spaces_categories = list(
+        map(lambda x: x.strip(), select_categories)
+    )
+    data["categories"] = list(
+        filter(lambda x: x != "", remove_spaces_categories)
+    )
+
+    return data
+
+
+# REF: https://www.kite.com/python/answers/
+# how-to-remove-empty-strings-from-a-list-of-strings-in-python
 
 
 # Requisito 5
