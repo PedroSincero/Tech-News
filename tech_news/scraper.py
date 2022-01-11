516b81c08d5b07c0cbe7486d00388fd5e224b783
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2022-01-11 15:38:45 -0300

    fix - finalizando requisito 2 - [DONE]

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-# from parsel import Selector
+from parsel import Selector
 
 
 # Requisito 1
@@ -21,12 +21,24 @@ def fetch(url):
 
 
 # Requisito 2
+# #js-main > div > div > div.z--col.z--w-2-3 > div.tec--list.tec--list--lg ::a
+# .tec--list--lg > a ::attr(href)
 
 
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    # response = fetch(html_content)
+
+    selector = Selector(text=html_content)
+
+    href = selector.css(
+        ".tec--list--lg h3.tec--card__title > a ::attr(href)"
+    ).getall()
+    return href
 
 
+# result = fetch("https://www.tecmundo.com.br/novidades")
+
+# scrape_novidades(result)
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
