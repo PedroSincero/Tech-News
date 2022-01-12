263b1cd307676a02964758649f7568cc07fc4548
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2022-01-12 18:54:21 -0300

    fix - finalizando requisito 6 - [DONE]

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+import re
+from tech_news.database import db
+
 # Requisito 6
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+
+    find_title = list(
+        db.news.find({"title": re.compile(title, re.IGNORECASE)})
+    )
+    result = []
+    for search_title in find_title:
+        result.append((search_title["title"], search_title["url"]))
+    return result
 
 
 # Requisito 7
